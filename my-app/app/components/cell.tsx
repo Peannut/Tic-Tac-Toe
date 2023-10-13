20c2686dc276274f6fe18f88e12c051bede474be
Peannut <zakariaspam0@gmail.com>
2023-10-13 17:42:48 +0100

    done

--- my-app/app/components/cell.tsx
+++ my-app/app/components/cell.tsx
@@ -1,5 +1,52 @@
-const Cell = () => {
-	  return <div className="square"></div>;
+import { Dispatch, SetStateAction } from "react";
+
+type CellProps = {
+	go: string;
+	id :number;
+	setGo: Dispatch<SetStateAction<string>>;
+	cells: string[];
+	setCells: Dispatch<SetStateAction<string[]>>;
+	cell: string;
+	winingmessage: string;
+
+}
+
+const Cell = ( {go, setGo , id, cells, setCells, cell, winingmessage} : CellProps) => {
+
+	const handleCellchange = (celltochange : string) => {
+		let copyCells = [...cells];
+
+		copyCells[id] = celltochange;
+
+		setCells(copyCells);
+
+	}
+	
+	const handleClick = (e) => {
+		if (winingmessage){
+			return;
+		}
+
+		const notTaken = !cells[id];
+
+		if (notTaken)
+		{
+			if (go === "circle"){
+				handleCellchange("circle");
+				setGo("cross");
+			}
+			else if (go === "cross"){
+				handleCellchange("cross");
+				setGo("circle");
+			}
+		}
+
+
+	}
+
+	  return <div className="square" onClick={handleClick}>
+		<div className={cell} > {cell? (cell === "circle" ? "O" : "X") : ""} </div>
+	  </div>;
 }
 
 export default Cell;
--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,20 +1,69 @@
 "use client"
 import Image from 'next/image'
-import { useState } from 'react'
+import { use, useEffect, useState } from 'react'
 import Cell from './components/cell';
+
+const winingCombos = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
 
 export default function Home() {
 
   const [cells , setCells] = useState(["","","","","","","","",""]);
+
+  const [go, setGo] = useState("circle");
+  const [winingmessage, setWiningmessage] = useState("");
+  
+
+  useEffect(() => {
+    winingCombos.forEach((combo) => {
+      const circlewins = combo.every((cell) => cells[cell] === "circle");
+      const crosswins = combo.every((cell) => cells[cell] === "cross");
+      if (circlewins){
+        setWiningmessage("circle wins");
+      }
+      if (crosswins){
+        setWiningmessage("cross wins");
+      }
+
+    })
+  }, [cells])
+
+  useEffect(() => {
+    if (cells.every((cell) => cell !== "") && !winingmessage){
+      setWiningmessage("draw");
+    }
+  } , [cells, winingmessage])
+
+  
   return (
     <div className='container'>
       <div className='gameboard'>
        {cells.map((cell, index) => (
-          <Cell key={index} />
+          <Cell 
+          id={index} 
+          go={go} 
+          setGo={setGo} 
+          key={index} 
+          cells={cells} 
+          setCells={setCells} 
+          cell={cell}
+          winingmessage={winingmessage}
+          />
+
         )
        )}
 
       </div>
+      <div>{winingmessage}</div>
+      {!winingmessage && <div>{`it now ${go} turn!`}</div>}
     </div>
   )
 }
