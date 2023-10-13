8c585c005b85039a7d711ede860be45eae4ee730
Peannut <zakariaspam0@gmail.com>
2023-10-13 16:28:58 +0100

    init

---
+++ my-app/app/page.tsx
@@ -0,0 +1,20 @@
+"use client"
+import Image from 'next/image'
+import { useState } from 'react'
+import Cell from './components/cell';
+
+export default function Home() {
+
+  const [cells , setCells] = useState(["","","","","","","","",""]);
+  return (
+    <div className='container'>
+      <div className='gameboard'>
+       {cells.map((cell, index) => (
+          <Cell key={index} />
+        )
+       )}
+
+      </div>
+    </div>
+  )
+}
